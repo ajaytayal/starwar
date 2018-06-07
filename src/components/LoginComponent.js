e976b2a4c2f916deba9fc3b111d15344197ce252
Ajay Gupta <ajay.gupta@naukri.com>
2018-06-07 09:26:25 +0530

    commit heading

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -25,6 +25,7 @@ export default class Main extends React.Component{
   render(){
     return (
       <div>
+      <div>This is the form</div>
 	  	<LoginForm users = {this.props.users} />
       </div>
     )
